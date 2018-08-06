0ed24e32a4d5c2769afc5862d23250268687f66d
kusupudiswamy <39132487+kusupudiswamy@users.noreply.github.com>
2018-08-06 19:12:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 sudo cp Dockerfile /opt/swamy
 sudo cd /opt/swamy
 pwd
-sudo docker build -t dar /opt/swamy
+sudo docker build -t daring .
 sudo docker tag dar kusupudiswamy/waiting:file
 sudo docker push kusupudiswamy/waiting:file
